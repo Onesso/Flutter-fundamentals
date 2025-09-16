87620539d99496c9a924e2c5d65760fb372a755a
Onesso <odhiambofrank1965@gmail.com>
2025-09-16 23:33:46 +0300

    build a custome class to render the application

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,38 @@ void main() {
           backgroundColor: Colors.black,
           toolbarHeight: 1.0,
         ),
-        body: SafeArea(
-          bottom: false,
-          child: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Color(0xFF191414),
-                  Color.fromARGB(255, 3, 37, 15),
-                ],
-              ),
-            ),
-            child: const Center(
-              child: Text(
-                "compulsary name parameters",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
-              ),
+        body: GradientContainer(),
+      ),
+    ),
+  );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return SafeArea(
+      bottom: false,
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFF191414),
+              Color.fromARGB(255, 3, 37, 15),
+            ],
+          ),
+        ),
+        child: const Center(
+          child: Text(
+            "compulsary name parameters",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
             ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
